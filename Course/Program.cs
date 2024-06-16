d709f55e006920b2910e47d26e7ae3745e33e007
Rodrigogrisi <Rodricosta1995@gmail.com>
2024-06-16 01:34:59 -0300

    When a withdrawal method is performed, there is a polymorphism based on the superposition of the withdrawal method in the savings account that discounts plus two in relation to the base function.

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using Course.Entities;
 
 namespace Course
@@ -12,12 +13,11 @@ namespace Course
             //SUPERCLASSE GENERICA 
             List<Account> list = new List<Account>();
 
-            list.Add(new SavingsAccount(1001, "Alex", 350.0, 0.01));
-            list.Add(new BusinessAccount(1002, "Maria", 150.0, 400));
-            list.Add(new SavingsAccount(1003, "Bob", 950.0, 0.01));
-            list.Add(new BusinessAccount(1004, "Anna", 250.0, 500));
+            list.Add(new SavingsAccount(1001, "Alex", 500, 0.01));
+            list.Add(new BusinessAccount(1002, "Maria", 500, 400));
+            list.Add(new SavingsAccount(1003, "Bob", 500, 0.01));
+            list.Add(new BusinessAccount(1004, "Anna", 500, 500));
             
-
             double sum = 0;
 
             foreach (Account acc in list) {
@@ -25,7 +25,20 @@ namespace Course
                 sum += acc.Balance;
             }
 
-            Console.WriteLine(sum);
+            Console.WriteLine("Total Balance: R$ " + sum.ToString("F2", CultureInfo.InvariantCulture ));
+
+            foreach (Account acc in list) {
+                acc.Withdraw(10.0);
+            }
+
+            foreach (Account acc in list) {
+
+                Console.WriteLine("updated balance for account: "
+                    + acc.Number
+                    + " -  R$ "
+                    + acc.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+
         }
     }
 }
